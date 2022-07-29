7c31ce0bdbd269a0dfd62d495c6939fcbdf89ed2
Jet <67730137+jethall1@users.noreply.github.com>
2022-07-29 10:47:58 +1000

    Add terraform testing/onboarding files
    Adding 2 new directories
    - onboarding
    - terraform_test
    
    These will be apart of the onboarding process for the cloud development team. Where they will pull the file and test is their terraform is setup correctly.

---
+++ Teams_T2_2022/Cloud_Dev/onboarding/examples/terraform_test/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sit-22t2-breaking-capt-8718903"
+  region = "australia-southeast2"
+}
+
+resource "google_storage_bucket" "test-bucket" {
+  name          = "jets_test_bucket"
+  location      = "US"
+  force_destroy = true
+}
